7c8a329ac11ee232179f32508b5c3bea9d56edcb
alexandr.negrya <alexandr.negrya@langly.onmicrosoft.com>
2024-03-12 21:27:02 +0200

    write skeleton of basic funcs

---
+++ async_funcs.py
@@ -0,0 +1,13 @@
+import asyncio
+import aiohttp
+
+
+async def _get_facebook_users_by_query(loop):
+    pass
+
+async def _get_facebook_users_by_criteria(loop):
+    pass
+
+def get_facebook_users():
+    loop = asyncio.get_event_loop()
+    
