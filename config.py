3fda5137316ebccc1f614efb36f6b88f10f0f714
alexandr.negrya <alexandr.negrya@langly.onmicrosoft.com>
2024-03-12 21:27:42 +0200

    add project config module

---
+++ config.py
@@ -0,0 +1,16 @@
+from pyfacebook import GraphAPI
+from dotenv import load_dotenv
+import os
+import argparse
+
+
+load_dotenv()
+graph_api = GraphAPI(access_token=os.environ.get("FACEBOOK_GRAPH_API_ACCESS_TOKEN"))
+args_parser = argparse.ArgumentParser()
+args_parser.add_argument("--min-age", type=int, required=False)
+args_parser.add_argument("--max-age", type=int, required=False)
+args_parser.add_argument(
+    "--gender", type=str, choices=("male", "female"), required=False
+)
+args_parser.add_argument("--country", type=str, required=False)
+args_parser.add_argument("--city", type=str, required=False)
