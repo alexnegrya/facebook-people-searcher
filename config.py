5789f45a305c7c2504f69e04b549f279eb749e92
alexnegrya <alexnegrya@mail.ru>
2024-03-20 20:58:51 +0200

    dont use Graph API

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-from pyfacebook import GraphAPI
+# from aiofb import GraphAPI
 from dotenv import load_dotenv
 import os
 import argparse
 
 
 load_dotenv()
-graph_api = GraphAPI(access_token=os.environ.get("FACEBOOK_GRAPH_API_ACCESS_TOKEN"))
+# graph_api = GraphAPI(access_token=os.environ.get("FACEBOOK_GRAPH_API_ACCESS_TOKEN"))
 args_parser = argparse.ArgumentParser()
 args_parser.add_argument("--min-age", type=int, required=False)
 args_parser.add_argument("--max-age", type=int, required=False)
