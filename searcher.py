9d0b4b424e79becf7ba5f98f4cd58d31f78a0bdc
alexnegrya <alexnegrya@mail.ru>
2024-03-26 23:12:36 +0200

    change skeleton of module

--- searcher.py
+++ searcher.py
@@ -1,30 +1,24 @@
-import asyncio
-import aiohttp
-# from aiofb import GraphAPI
 import string
+import mouse
 
 
-_SUPPORTED_CRITERIA = ('min_age', 'max_age', 'gender', 'country', 'city')
+_SUPPORTED_ARGS = ('min_age', 'max_age', 'gender', 'country')
 
 
 class PeopleSearcher:
     def __init__(self):
-        # self.graph_api = graph_api
-        self.loop = asyncio.get_event_loop()
-        self.letters = tuple(string.ascii_lowercase)
+        self.letters = list(string.ascii_lowercase) + list('бвгдёжзийклмнпртуфхцчшщъыьэюя')
         self.letter_index = 0
     
-    def __is_user_duplicate(self, user):
-        pass
-    
-    def __get_users_by_current_letter(self):
-        # users = self.loop.run_until_complete(self.graph_api.get("/search", {
-        #     "q": self.letters[self.letter_index], "limit": 10}))
+    def __update_users_by_current_letter(self):
         self.letter_index += 1
-    
-    def __filter_users_by_criteria(self, users, criteria):
+
+    def __is_user_duplicate(self):
         pass
 
-    def search_users(self, **criteria):
-        if any([c not in _SUPPORTED_CRITERIA for c in criteria]):
-            raise ValueError('these criteria are not supported')
+    def __is_suitable_user(self):
+        pass
+
+    def search_users(self, **args):
+        if any([c not in _SUPPORTED_ARGS for c in args]):
+            raise ValueError('these args are not supported')
