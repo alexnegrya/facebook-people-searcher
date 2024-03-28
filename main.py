0895ffd6df44f19ec4fe51cf1205de7e77a9019a
alexnegrya <alexnegrya@mail.ru>
2024-03-28 22:15:57 +0200

    fix first bugs

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from positions import save_side_position, save_element_positions
 
 
 def set_criteria(name: str, msg: str):
-    criteria = SearchCriteria.get_or_create(name=name)
+    criteria = SearchCriteria.get_or_create(name=name)[0]
     value = input(f'{msg} > ')
     if len(value) != 0:
         criteria.value = value
@@ -15,41 +15,43 @@ def set_criteria(name: str, msg: str):
 
 def set_button_positions(name: str):
     input(f'Put your cursor in the center of the Facebook`s \
-        "{name[0].upper()}{name[1:]}" button > ')
-    save_element_positions(name)
+"{name[0].upper()}{name[1:]}" button > ')
+    save_element_positions(ElementPositions, name)
 
 
 if __name__ == "__main__":
-    input('Open following link from browser in which you are logged in to your \
-        Facebook account - https://www.facebook.com/search/people/?q=a > ')
-    
     if len(BrowserElementPositions.select()) == 0:
         input('Put your cursor on the address bar in your browser > ')
         save_element_positions(BrowserElementPositions, 'address_bar')
         input('Put your cursor on the back to previous web page button in your \
-            browser > ')
+browser > ')
         save_element_positions(BrowserElementPositions, 'back')
     
     while True:
         answer = input('Do you want to set/change search criteria? (y/n) > ')
         if answer == 'y':
             print('Note: For these criteria will be used values which will appear \
-                first in the Facebook`s input of criteria.')
+first in the Facebook`s input of criteria.')
             print('Hint: Leave blank answers for criteria to do not set values.')
             set_criteria('friends', 'Which friends must have people you \
-                searching (my fiends/friends of friends/{exact full name of \
-                your friend})')
+searching (my friends/friends of friends/{exact full name of \
+your friend})')
             set_criteria('city', 'In which city you want to search people \
-                ({city name}, {country name})')
+({city name}, {country name})')
             set_criteria('education', 'Exact name of educational institution \
-                in which people you searching were learned')
+in which people you searching were learned')
             set_criteria('work', 'Exact company name in which work people you searching')
+            break
         elif answer == 'n':
+            if len(SearchCriteria.select()) == 0:
+                [SearchCriteria.create(name=name) for name in SEARCH_CRITERIA]
             break
 
+    input('Open following link from browser in which you are logged in to your \
+Facebook account - https://www.facebook.com/search/people/?q=a > ')
     if all([len(m.select()) == 0 for m in (SidePosition, ElementPositions)]):
         input('Put your cursor on the up side of the Facebook website in tab with \
-            opened link in your browser > ')
+opened link in your browser > ')
         save_side_position('upper')
         input('Put your cursor on the down side of the same Facebook website page > ')
         save_side_position('bottom')
@@ -58,13 +60,12 @@ if __name__ == "__main__":
         [set_button_positions(name) for name in (
             'people', 'friends', 'city', 'education', 'work')]
         input('Put your cursor on the name of the first person who appeared in \
-            the Facebook`s search result > ')
+the Facebook`s search result > ')
         save_element_positions(ElementPositions, 'person_name')
-    else:
-        print('Use the same web page scale and open it with the same browser \
-            and extensions to prevent wrong behaviour of this program.')
+
+    # open first user page and save user data elements positions like in previous block of code
 
     args = args_parser.parse_args()
     searcher = PeopleSearcher()
     users = searcher.search_users(**args)
-    print(users)
+    # print(users)
