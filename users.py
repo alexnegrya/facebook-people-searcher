0f7216979d31b9779c71f70d08ff404345f8eb3f
alexnegrya <alexnegrya@mail.ru>
2024-03-28 22:12:15 +0200

    write class for users data storing

--- users.py
+++ users.py
@@ -1,2 +1,15 @@
 class User:
-    pass
+    def __init__(self, url: str, full_name: str, age: int, gender: str, country: str, possible_duplicate=False):
+        self.url = url
+        self.full_name = full_name
+        self.age = age
+        self.gender = gender
+        self.country = country
+        
+        self.possible_duplicate = possible_duplicate
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        return self.__str__()
